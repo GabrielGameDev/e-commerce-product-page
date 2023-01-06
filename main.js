6dc647d398ec1af889421be07507fd48b0181446
GabrielGameDev <gabrielgamedev@gmail.com>
2023-01-06 18:34:44 -0300

    Modal do carrinho

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const btn_plus = document.getElementById('btn_plus');
 const amountText = document.getElementById('amount');
 const cartNumber = document.getElementById('cart-number');
 
+const cardText = "<div class='d-flex justify-content-center align-items-center gap-2'><img src='images/image-product-1-thumbnail.jpg'  alt=''  width='40px'  height='40px'  class='rounded'/><p class='card-text'>  Fall Limited Edition Sneakers $125.00 x 3  <span class='fw-bolder'>$375.00</span></p><img src='images/icon-delete.svg' alt='' /></div><button class='btn btn-warning mx-auto my-3 w-100 py-3 bg-dark-orange text-white'>Checkout</button>"
+const cardTextEmpty = "Your cart is empty"
+const cardCart = document.querySelector('.card');
+const cardBody = document.querySelector('.card-body');
+let cardVisible = false;
+
 let amount = 0
 
 btn_less.addEventListener('click', () => {
@@ -13,11 +19,15 @@ btn_less.addEventListener('click', () => {
     if (amount < 0) {
         amount = 0;
     }
+    if(cardVisible)
+        showCart()
     amountText.innerHTML = amount;
 });
 
 btn_plus.addEventListener('click', () => {
     amount += 1;
+    if(cardVisible)
+        showCart()
     amountText.innerHTML = amount;
 });
 
@@ -31,5 +41,23 @@ function addToCart() {
         cartNumber.classList.add('d-none');
     }
 
-    
+    if(cardVisible)
+    showCart()
+}
+
+function showCart(){
+    cardVisible = !cardVisible;
+    if (amount > 0) {
+        cardBody.innerHTML = cardText;
+    } else {
+        cardBody.innerHTML = cardTextEmpty;
+    }
+    if (cardVisible) {
+        cardCart.classList.remove('d-none');
+        cardCart.classList.add('d-block');
+    }
+    else {
+        cardCart.classList.remove('d-block');
+        cardCart.classList.add('d-none');
+    }
 }
