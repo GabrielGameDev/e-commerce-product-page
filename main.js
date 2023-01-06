6306d5f8c2eba9410dd038deee965b4996bdfa4c
GabrielGameDev <gabrielgamedev@gmail.com>
2023-01-06 16:06:55 -0300

    Botões de aumentar e diminuir

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const btn_less = document.getElementById('btn_less');
+const btn_plus = document.getElementById('btn_plus');
+const amountText = document.getElementById('amount');
+
+let amount = 0
+
+btn_less.addEventListener('click', () => {
+    amount -= 1;
+    if (amount < 0) {
+        amount = 0;
+    }
+    amountText.innerHTML = amount;
+});
+
+btn_plus.addEventListener('click', () => {
+    amount += 1;
+    amountText.innerHTML = amount;
+});
